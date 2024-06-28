29a13b761de8ba4da81c53de5ee4ba87e8b48e2e
SeonilChoi <seonilchoi98@gmail.com>
2024-06-28 17:27:51 +0900

    Update MPU9250SerialCommunication

---
+++ Arduino/MPU9250SerialCommunication/MPU9250SerialCommunication.ino
@@ -0,0 +1,27 @@
+#include "SerialHandler.h"
+#include "MPU9250.h"
+
+#define BAUDRATE 9600
+
+SerialHandler serial_handler;
+MPU9250       mpu_9250;
+
+void setup() {
+  serial_handler.openPort(BAUDRATE);
+  mpu_9250.setupSensor();
+}
+
+void loop() {
+  uint8_t read_data[4];
+  uint8_t read_length = 4;
+  
+  int result = serial_handler.readPort(read_data, read_length);
+  if (result != COMM_SUCCESS)
+    return;
+  
+  uint8_t imu_data[18];
+  uint8_t id_length = read_data[0];
+  mpu_9250.readSensor(read_data + 1, id_length, imu_data);
+
+  result = serial_handler.writePort(imu_data, 18);
+}
